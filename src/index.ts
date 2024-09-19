7a1eec805c8419f4da9e46b858d652f41c032f1e
Miguel Ángel Bonilla Fernández <mbonfer2608@cpifpalanturing.es>
2024-09-19 14:36:51 +0200

    Segunda subida

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import * as readline from 'readline-sync';
 function main(){
     console.log("=== Gestor de Alumnos y sus respectivas notas ===")
 
+    const options = ['Crear Usuario', 'Asignar Materia a un alumno', 'Listar materias de un alumno', 'Crear una materia', 'Listar materias', 'Asignar nota de una materia a un alumno', 'Listar alumnos con nota']
+
     let name: string = readline.questionFloat('Ingrese el nombre del alumno: ')
     let surname: string = readline.questionFloat('Ingrese el nombre del alumno: ')
 
-
+    
 }
